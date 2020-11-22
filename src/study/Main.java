ef952dedf5bcb0b340b7a5d8c70e7cf7ad31713a
maks nakov <maksnakov@gmail.com>
2020-11-22 20:53:23 +0300

    change some comments

--- src/study/Main.java
+++ src/study/Main.java
@@ -11,7 +11,7 @@ public class Main {
         Scanner mult = new Scanner(System.in );
         From.randomNumber = mult.nextDouble();
 
-        System.out.println("Перевод числа 456 в 2ую систему: " + Integer.toBinaryString((int) From.randomNumber));
+        System.out.println("Перевод числа " + From.randomNumber + "в 2ую систему: " + Integer.toBinaryString((int) From.randomNumber));
 
         System.out.println("12ую систему: " + Integer.toString((int) From.randomNumber, 12) + ", 12 - мой порядковый номер");
 
