7ed68b9e4d8af857d0a13eeaf16d32b36c9edd90
maks nakov <maksnakov@gmail.com>
2020-11-22 15:30:57 +0300

    change some comments

--- src/study/Main.java
+++ src/study/Main.java
@@ -11,11 +11,11 @@ public class Main {
         Scanner mult = new Scanner(System.in );
         From.randomNumber = mult.nextDouble();
 
-        System.out.println("Перевод чисоа 456 в 2ую систему: " + Integer.toBinaryString((int) From.randomNumber));
+        System.out.println("Перевод числа 456 в 2ую систему: " + Integer.toBinaryString((int) From.randomNumber));
 
-        System.out.println("12ую систему: " + Integer.toString((int) From.randomNumber, 12) + " 12 - мой порядковый номер");
+        System.out.println("12ую систему: " + Integer.toString((int) From.randomNumber, 12) + ", 12 - мой порядковый номер");
 
-        System.out.println("16ую систеуа: " + Integer.toHexString((int) From.randomNumber));
+        System.out.println("16ую система: " + Integer.toHexString((int) From.randomNumber));
         System.out.println("Ты красавчик!!");
 
 
